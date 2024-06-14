0632fc998531fd9000d07c800631682c0fea4074
Kodandarao Chellapilli <briggs2019@go.byuh.edu>
2024-06-13 19:24:00 -0600

    added random letters for the first part

--- password.py
+++ password.py
@@ -6,3 +6,18 @@ numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
 print("Welcome to the PyPassword Generator!")
+
+nr_letters =  int(input("How many letters would you like in your password?\n"))
+nr_symbols = int(input(f"How many symbols would you like?\n"))
+nr_numbers = int(input(f"How many numbers would you like?\n"))
+
+#Eazy Level  
+
+
+#Hard Level
+
+password = ""
+#nr_letters  = 4 = 
+for char in range(1, nr_letters+1):
+  random_char = random.choice(letters)
+  print(random_char)
