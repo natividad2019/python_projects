e98ca5b0011802595c014616202d5df51455f7e3
Kodandarao Chellapilli <briggs2019@go.byuh.edu>
2024-06-13 19:32:12 -0600

    added all three parts of the code

--- password.py
+++ password.py
@@ -16,8 +16,23 @@ nr_numbers = int(input(f"How many numbers would you like?\n"))
 
 #Hard Level
 
-password = ""
+
 #nr_letters  = 4 = 
-for char in range(1, nr_letters+1):
-  random_char = random.choice(letters)
-  print(random_char)
+password_list = []
+
+for char in range(1, nr_letters + 1):
+  password_list.append(random.choice(letters))
+
+for char in range(1, nr_symbols + 1):
+  password_list += random.choice(symbols)
+
+for char in range(1, nr_numbers + 1):
+  password_list += random.choice(numbers)
+
+random.shuffle(password_list)
+
+password = ""
+for char in password_list:
+  password += char
+
+print(f"Your password is: {password}")
